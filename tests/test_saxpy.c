d5768a1774fec9282aebf5aec03263084bcac870
elaineal <elainealicel@yahoo.com>
2016-05-12 02:14:32 -0500

    saxpy test edit

--- tests/test_saxpy.c
+++ tests/test_saxpy.c
@@ -1,29 +1,13 @@
 #include <time.h>
 #include <math.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
 #include "/opt/acml5.3.0/gfortran64/include/acml.h"
 
-extern	void	xsaxpy_(	const int*n, const float *alpha, float *f_x, const int*incx, float *f_y, const int* incy);
-
-// C
-void c_saxpy(const int n, const float alpha, float *x, const int incx, float *y, const int incy)
-{
-	register int i;
-	
-	if(incx == 1 && incy == 1)
-	{
-		for(i = 0; i < n; ++i) 
-		{
-			y[i] = y[i] + alpha*x[i];
-		}
-	}
-
-	
-}
+extern	void	xsaxpy_( const int*n, const float *alpha, float *f_x, const int*incx, float *f_y, const int* incy);
 
 
 // Fortran 
@@ -34,97 +18,105 @@ void f_saxpy(int n, float alpha, float *x, int incx, float *y, int incy)
 
 int N, incx, incy;
 float alpha;
-float *x,*y, *t_y;
+float *x, *y_1, *y_2,*t_y;
 
 struct timespec begin, end;
 unsigned long long int acml_time()
 {
-	memset(x,0,sizeof(float)*N);
-	memset(y,0,sizeof(float)*N);
-	memcpy(y,t_y,sizeof(float)*N);
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	saxpy(N,alpha,x,incx,y,incy);
-	clock_gettime(CLOCK_MONOTONIC, &end);
-        unsigned long long int time = 1000000000L*(end.tv_sec - begin.tv_sec) + end.tv_nsec - begin.tv_nsec;
-        printf("%32lld",time);
-        return time;
-}
+  memcpy(y_1,t_y,sizeof(float)*N);
 
-unsigned long long int c_time()
-{
+  clock_gettime(CLOCK_MONOTONIC,&begin);
+  saxpy(N,alpha,x,incx,y_1,incy);
+  clock_gettime(CLOCK_MONOTONIC,&end);
 
-	memset(x,0,sizeof(float)*N);
-	memset(y,0,sizeof(float)*N);
-	memcpy(y,t_y,sizeof(float)*N);
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	c_saxpy(N,alpha, x,incx,y,incy);
-	clock_gettime(CLOCK_MONOTONIC, &end);
-        unsigned long long int time = 1000000000L*(end.tv_sec - begin.tv_sec) + end.tv_nsec - begin.tv_nsec;
-        printf("%32lld",time);
-        return time;
+  unsigned long long int time = 1000000000L*(end.tv_sec - begin.tv_sec) + end.tv_nsec - begin.tv_nsec;
+  printf("%32lld",time);
+  fflush(stdout);
+  return time;
 }
 
 
 unsigned long long int f_time()
 {
-	memset(x,0,sizeof(float)*N);
-	memset(y,0,sizeof(float)*N);
-	memcpy(y,t_y,sizeof(float)*N);
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	f_saxpy(N,alpha,x,incx,y,incy);
-	clock_gettime(CLOCK_MONOTONIC, &end);
-        unsigned long long int time = 1000000000L*(end.tv_sec - begin.tv_sec) + end.tv_nsec - begin.tv_nsec;
-        printf("%32lld",time);
-        return time;
+  memcpy(y_2,t_y,sizeof(float)*N);
+
+  clock_gettime(CLOCK_MONOTONIC,&begin);
+  f_saxpy(N,alpha,x,incx,y_2,incy);
+  clock_gettime(CLOCK_MONOTONIC,&end);
+
+  unsigned long long int time = 1000000000L*(end.tv_sec - begin.tv_sec) + end.tv_nsec - begin.tv_nsec;
+  printf("%32lld",time);
+  return time;
+}
+
+int array_cmp()
+{
+  int n1 = memcmp(y_1,y_2,sizeof(float)*N);
+
+  return !n1;
+}
+
+void run_test() 
+{
+  printf("Running test for SAXPY with N = %d, incx = %d, incy = %d, alpha = %d\n ", N, incx, incy, alpha);
+  printf("%32s%32s\n","ACML", "Fortran");
+  printf("Trials\n");
+  printf("%32s%32s%32s\n", "Time (ns)", "Time (ns)", "ACML == FT");
+
+  int c = 8, it = 0;
+  unsigned long long int tot1 = 0, tot2 = 0;
+
+  int ic;
+  for (ic = 0; ic < c; ic++)
+  {
+    unsigned long long int t1, t2;
+    t1 = acml_time();
+    t2 = f_time();
+    printf("%32d", array_cmp());
+    if (ic >= 2)
+    {
+      tot1 += t1;
+      tot2 += t2;
+      ++it;
+    }
+    printf("\n");
+  }
+
+  printf("Average (Does not include first two trials)\n");
+  printf("%32llu%32llu\n",tot1/it,tot2/it);
+  printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
-	init_();    
-	srand(time(NULL));
-   	
-	N = atoi(argv[1]);
-    	incx = 1;
-	incy = 1;
+  srand(time(NULL));
 
-	alpha = rand()/1.0/RAND_MAX - 0.5;
-    	x = (float*)malloc(sizeof(float)*N);
-    	y = (float*)malloc(sizeof(float)*N);
-    	t_y = (float*)malloc(sizeof(float)*N);
-	
-	int i;
-    	for (i = 0; i < N; i++) {
-       		x[i] = rand()/1.0/RAND_MAX - 0.5;
-       		t_y[i] = rand()/1.0/RAND_MAX - 0.5;
-	}
-	
-        int c = 8, ic, it = 0;
-	
-	printf("Running test for SAXPY with N = %d, alpha = %d\n", N, alpha);
-        printf("%32s%32s%32s\n","ACML","C", "FORTRAN");
-	printf("Trials\n");
-	printf("%32s%32s%32s\n", "Time (ns)", "Time (ns)", "Time (ns)");
+  N = atoi(argv[1]);
 
-        unsigned long long int tot1 = 0, tot2 = 0, tot3 = 0;
-        for (ic = 0; ic < c; ic++)
-        {
-               unsigned long long int t1, t2, t3;
-                t1 = acml_time();
-                t2 = c_time();
-		t3 = f_time();
+  alpha = rand()/1.0/RAND_MAX - 0.5;
+  x = (float*)malloc(sizeof(float)*N);
+  y_1 = (float*)malloc(sizeof(float)*N);
+  y_2 = (float*)malloc(sizeof(float)*N);
+  t_y = (float*)malloc(sizeof(float)*N);
 
-                if (ic >= 2)
-                {
-                        tot1 += t1;
-                        tot2 += t2;
-                        tot3 += t3;
-                        it ++;
-                }
-                printf("\n");
-        }
-        
-	printf("Average\n");
-        printf("%32llu%32llu%32llu\n",tot1/it,tot2/it,tot3/it);
-    
-	return 0;
+  int i;
+  for (i = 0; i < N; i++)
+  {
+    t_y[i] = rand()/1.0/RAND_MAX - 0.5;
+    x[i] = rand()/1.0/RAND_MAX - 0.5;
+  }
+
+  incx = 1;
+  incy = 1;
+  run_test();
+
+  incx = 2;
+  incy = 2;
+  run_test();
+
+  incx = -2;
+  incy = -2;
+  run_test();
+
+  return 0;
 }
